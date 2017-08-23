18cbc20b1120175750d5c18466bd914480b2a586
przemwo <przemwo@o2.pl>
2017-08-23 10:41:00 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Simple starter project for JavaScript
+
+To run webpack-dev-server on localhost:3000:
+```sh
+$ npm start
+```
+To build project in dist directory:
+```sh
+$ npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        use: {
+          loader: 'babel-loader'
+        }
+
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Starter',
+      template: 'index.ejs'
+    })
+  ]
+};
